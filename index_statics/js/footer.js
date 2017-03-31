b25ae42f0c37f90e95303c7d21d712d49e4f5441
ksyydream <249499109@qq.com>
2017-03-31 09:31:12 +0800

    2312

---
+++ index_statics/js/footer.js
@@ -0,0 +1,18 @@
+document.writeln("<div class=\"footer\">");
+document.writeln("	<ul class=\"footerList\">");
+document.writeln("		<li><a href=\"aboutMaxi.html\">关于淼溪</a></li>");
+document.writeln("		<li><a href=\"product.html\">公司产业</a></li>");
+document.writeln("		<li><a>净水器服务</a></li>");
+document.writeln("		<li><a href=\"contact.html\">联系我们</a></li>");
+document.writeln("		<li><a>关注微信公众号</a></li>");
+document.writeln("		<li><a>下载APP</a></li>");
+document.writeln("	</ul>");
+document.writeln("	<div class=\"qr_code\">");
+document.writeln("		<img src=\"../img/weiChart.png\" alt=\"\" class=\"weiChart\">");
+document.writeln("		<img src=\"../img/APPCode.png\" alt=\"\" class=\"APP\">");
+document.writeln("	</div>");
+document.writeln("	<p class=\"beian\">");
+document.writeln("		地址：上海市嘉定区金沙西路1555弄慧创国际395号");
+document.writeln("	</p>");
+document.writeln("	<p class=\"beian\">&copy;Copyright 2016 淼溪信息科技发展（上海）有限公司 版权所有 备案号：沪ICP备14013198号-3</p>");
+document.writeln("</div>");
