1ac0588e2399dae60d528e5c4a15f84a3fd8a034
ksyydream <249499109@qq.com>
2017-04-06 10:41:42 +0800

    2312

--- admin/controllers/Jobs.php
+++ admin/controllers/Jobs.php
@@ -10,7 +10,7 @@ class Jobs extends MY_Controller {
 
 	public function list_job($page=1){
 		$data = $this->job_model->list_job($page);
-		$base_url = "/admin.php/AboutMaxi/list_job/";
+		$base_url = "/admin.php/Jobs/list_job/";
 		$pager = $this->pagination->getPageLink($base_url, $data['total'], $data['limit']);
 		$this->assign('pager', $pager);
 		$this->assign('data', $data);
@@ -18,45 +18,23 @@ class Jobs extends MY_Controller {
 		$this->show('job/list_job');
 	}
 
-	public function upload_image(){
-		$dir = FCPATH . '/upload/news_logo';
-		if(!is_dir($dir)){
-			mkdir($dir,0777,true);
-		}
-		$config['upload_path'] = './upload/news_logo/';
-		$config['allowed_types'] = 'gif|jpg|png|jpeg';
-		$config['encrypt_name'] = true;
-		$config['max_size'] = '3200';
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload('userfile')){
-			echo 1;
-		}else{
-			$pic_arr = $this->upload->data();
-			echo $pic_arr['file_name'];
-		}
-	}
-
-	public function save_news(){
+	public function save_job(){
 		if(!$this->input->post('title')){
 			$this->show_message('新闻标题不能为空!');
 		}
-		if(!$this->input->post('logo')){
-			$this->show_message('新闻logo不能为空!');
-		}
-		$rs =$this->news_model->save_news();
+		$rs =$this->job_model->save_news();
 		if($rs == 1){
-			$this->show_message('保存成功',site_url('news/list_news'));
+			$this->show_message('保存成功',site_url('jobs/list_job'));
 		}else{
 			$this->show_message('保存失败');
 		}
 	}
-	public function add_news($id=null,$page=1){
+	public function add_job($id=null,$page=1){
 		$this->assign('page', $page);
 		if($id){
-			$detail = $this->news_model->get_new_detail($id);
+			$detail = $this->job_model->get_job_detail($id);
 			$this->assign('detail', $detail);
 		}
-		$this->show('news/news_detail');
+		$this->show('job/job_detail');
 	}
 }
